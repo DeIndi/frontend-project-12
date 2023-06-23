9af775afd553497f33ade514e093b225979671b1
Nikolay Rogozin <norogozin@gmail.com>
2023-06-23 18:28:59 +0400

    useState removed from Chat component

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import { useAuth, useSocketAPI } from "../hooks";
 import routes from "../routes";
