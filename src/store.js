c7bace5e75b7c3dc12211ae0002e1315099c58fd
Nikolay Rogozin <norogozin@gmail.com>
2023-07-27 04:51:51 +0400

    using only redux-toolkit in store.js now

--- src/store.js
+++ src/store.js
@@ -1,18 +1,16 @@
-import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './slices/channelsSlice';
 import messagesReducer from './slices/messagesSlice';
 import modalsReducer from './slices/modalsSlice';
 
-// TODO: здесь стоит использовать redux-toolkit
-const rootReducer = combineReducers({
-  channels: channelsReducer,
-  messages: messagesReducer,
-  modals: modalsReducer,
-});
+// TODO: здесь стоит использовать redux-toolkit (fixed)
 // TODO: вернуть configureStore (fixed)
-const store = configureStore(
-  { reducer: rootReducer },
-);
+const store = configureStore({
+  reducer: {
+    channels: channelsReducer,
+    messages: messagesReducer,
+    modals: modalsReducer,
+  },
+});
 
 export default store;
